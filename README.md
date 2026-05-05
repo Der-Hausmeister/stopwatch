54456c32fe74924a4c1f41a50fd8b7ab526bacb3
Lars Roesicke <lars.roesicke1127@gmail.com>
2026-05-05 15:38:13 +0200

    Add static serving of stopwatch code
    I wanted to add something that starts the application in a web-browser. This way, it is easier to start in later on without an IDE. I generated this stuff with AI, and it suggested using Express. Seems simple enough to me.

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 This is a simple stopwatch I coded to practice my HTML,CSS and JavaScript skills. I coded this with the guidance of this
 Hyperskill project: https://hyperskill.org/projects/448
+
+I used Express to serve the stopwatch-files statically. One has to run this command to start the server:
+```js
+npm start
+```
--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Happy developing ✨');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'app')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'app', 'stopwatch.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
